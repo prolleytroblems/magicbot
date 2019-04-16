1fe7eb1ff516563c390dd65ad2e6bb4f614bb766
Lucas N <luknak@hotmail.com>
2019-04-16 21:11:17 +0900

    -a

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+import time
+import json
+
+from flask import Flask, jsonify, make_response
+import requests
 
 app = Flask(__name__)
 
 
 
-@app.route('/')
-def home():
-    return 'fuck me'
+@app.route('/<string:cardname>')
+def home(cardname):
+    response = requests.get("https://api.scryfall.com/cards/search?q="+cardname.replace(" ", "%20"))
+    if response.ok:
+        time.sleep(0.1)
+        image_url = json.loads(response.content)["data"][0]["image_uris"]["normal"]
+        response = requests.get(image_url)
+        response = make_response(response.content)
+        response.headers.set('Content-Type', 'image/png')
+        response.headers.set('Content-Disposition', 'attachment', filename='card.png')
+        return response
+    else:
+        return "Womp womp"
 
 if __name__=="__main__":
     app.run()
