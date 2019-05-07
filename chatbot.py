490e01ec7e488bdd3cda638f3b965ffa052b0297
Lucas N <luknak@hotmail.com>
2019-05-07 21:25:59 +0900

    bug found but not yet fixed

--- chatbot.py
+++ chatbot.py
@@ -6,9 +6,9 @@ import hmac
 def verify_source(body_bytestr, headers, channel_secret):
     hash = hmac.new(channel_secret.encode('utf-8'), body_bytestr, hashlib.sha256).digest()
     signature = base64.b64encode(hash)
-    return signature==headers['X-Line-Signature'].decode('utf-8')
+    return signature==headers['X-Line-Signature'].encode('utf-8')
 
-#def message(body_bytestr, headers):
-    #assert headers['type']=='message'
+def message(body_bytestr, headers):
+    assert headers['type']=='message'
 
-    #reply_token = headers['replyToken']
+    reply_token = headers['replyToken']
