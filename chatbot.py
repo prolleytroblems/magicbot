64da5211702e7c2577d4d4cf1ea8e04c5a8df2ef
Lucas N <luknak@hotmail.com>
2019-05-07 21:08:06 +0900

    add verification and import dammit last time

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,7 @@ import hmac
 def verify_source(request_obj, channel_secret):
     #Make sure the body is not too big (100kB limit)
     assert request_obj.content_length < 100000
-    body = request.get_data()
+    body = request_obj.get_data()
     hash = hmac.new(channel_secret.encode('utf-8'), body, hashlib.sha256).digest()
     signature = base64.b64encode(hash)
 
