294f0cb9c4b6b0e95870e3f285ccf56e6150c79f
Lucas N <luknak@hotmail.com>
2019-05-20 20:16:20 +0900

    debug dnd rolls

--- linednd.py
+++ linednd.py
@@ -7,8 +7,8 @@ def roll(rollargs):
         rollargs[0]=1
     for _ in range(int(rollargs[0])):
         out += int(random.random()*int(rollargs[1]))+1
-    if int(rollargs[3]) == '+':
+    if rollargs[3] == '+':
         out = out + int(rollargs[4])
-    elif int(rollargs[3]) == '-':
+    elif rollargs[3] == '-':
         out = out - int(rollargs[4])
     return out
