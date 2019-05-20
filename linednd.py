eb588c3b13dfa6a8efbc13f878460898c706afda
Lucas N <luknak@hotmail.com>
2019-05-20 20:10:24 +0900

    debug dnd rolls

--- linednd.py
+++ linednd.py
@@ -2,7 +2,7 @@ import random
 
 def roll(rollargs):
     out = 0
-    if rollargs[0]==''
+    if rollargs[0]=='':
         rollargs[0]=1
     for _ in range(int(rollargs[0])):
         out += int(random.random()*int(rollargs[1]))+1
